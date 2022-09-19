b5cd786bd4dc6614be7a9f50f91b077e00bedb97
swoutch <38116812+swoutch@users.noreply.github.com>
2022-09-19 14:01:28 +0200

    remove useless code

--- background.js
+++ background.js
@@ -1,18 +1,4 @@
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
-
 chrome.action.onClicked.addListener(async (tab) => {
-  const currentTab = await getCurrentTab();
-  console.log(currentTab);
-  const newUrl = `https://google.com/search?q=cache:${currentTab.url}`;
-  console.log(newUrl)
-  await chrome.tabs.update(currentTab.id, { url: newUrl })
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content.js']
-  });
+  const newUrl = `https://google.com/search?q=cache:${tab.url}`;
+  await chrome.tabs.update(tab.id, { url: newUrl })
 });
