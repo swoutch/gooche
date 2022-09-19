51d07750a9b0601b1a4d8fad5fa63ea3524a3cfc
swoutch <38116812+swoutch@users.noreply.github.com>
2022-09-19 14:10:50 +0200

    remove useless arg

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.action.onClicked.addListener(async (tab) => {
   const newUrl = `https://google.com/search?q=cache:${tab.url}`;
-  await chrome.tabs.update(tab.id, { url: newUrl })
+  await chrome.tabs.update({ url: newUrl })
 });
