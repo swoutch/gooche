1a720be80e9060b5d1d81636435054e4e04d042b
swoutch <38116812+swoutch@users.noreply.github.com>
2022-09-19 13:49:58 +0200

    log current tab

--- background.js
+++ background.js
@@ -1,4 +1,12 @@
-chrome.action.onClicked.addListener((tab) => {
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+chrome.action.onClicked.addListener(async (tab) => {
+  console.log(await getCurrentTab());
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['content.js']
